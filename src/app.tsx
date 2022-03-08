1f2aaa19a6296b0bd1cd76efcb3e1a08b99378c5
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-03-08 10:01:45 -0300

    feat: added styled-components

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,19 @@
+import styled from 'styled-components'
+
 export function App () {
   return (
-    <>
-      <h1>TGL</h1>
+    <Wrapper>
+      <Heading>TGL</Heading>
       <h2>The greatest app for Lottery</h2>
-    </>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  font-family: Helvitica, sans-serif;
+`
+
+const Heading = styled.h1`
+  font-size:3rem;
+  color: #B5C401;
+`
