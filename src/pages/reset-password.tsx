e10ffbd6337b16dbc83089b76ea6f31b3a608042
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-03-09 14:17:04 -0300

    feat(ResetPasswordPage): added content

--- src/pages/reset-password.tsx
+++ src/pages/reset-password.tsx
@@ -1,6 +1,29 @@
+import { Form } from '@/components/form'
+import { Input } from '@/components/input'
+import { ButtonLink } from '@/components/button-link'
+
+import {
+  AiOutlineArrowRight as ArrowRightIcon,
+  AiOutlineArrowLeft as ArrowLeftIcon,
+} from 'react-icons/ai'
+
+import * as S from './styles'
+
 function ResetPasswordPage () {
   return (
-    <h1>Reset Password Page</h1>
+    <>
+      <S.Heading>Reset password</S.Heading>
+      <Form>
+        <Input placeholder='Email' />
+
+        <S.ButtonLinkWrapper>
+          <ButtonLink as='button' color='greenLight'>
+            Send link <ArrowRightIcon />
+          </ButtonLink>
+        </S.ButtonLinkWrapper>
+      </Form>
+      <ButtonLink as='button'><ArrowLeftIcon /> Back</ButtonLink>
+    </>
   )
 }
 
