47636bf179a653cfa3758c93cd30c445c45302db
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-03-13 18:54:37 -0300

    feat(api): added interceptors for requests

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,33 @@
 import axios from 'axios'
 
-export const api = axios.create({
+const api = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL as string,
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
   },
 })
+
+const getAuthTokenLocalStorage = (): unknown => {
+  const storage = localStorage.getItem('@tgl/authentication')
+  if (storage) {
+    return JSON.parse(storage)
+  }
+
+  return null
+}
+
+api.interceptors.request.use(config => {
+  const auth = getAuthTokenLocalStorage() as {token: string}
+  if (!auth) {
+    return config
+  }
+
+  if (config.headers) {
+    config.headers.Authorization = `Bearer ${auth.token}`
+  }
+
+  return config
+})
+
+export { api }
