40077abc254a1c5889ed72418ce285ebe99b2099
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-03-13 18:09:28 -0300

    feat(RegisterPage): added submit of user register data to server

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,4 +1,8 @@
-import { MouseEvent } from 'react'
+import { FormEvent } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { api } from '@/services/api'
+import axios from 'axios'
+
 import { Form } from '@/components/form'
 import { Input } from '@/components/input'
 import { ButtonLink } from '@/components/button-link'
@@ -7,22 +11,51 @@ import {
   AiOutlineArrowRight as ArrowRightIcon,
   AiOutlineArrowLeft as ArrowLeftIcon,
 } from 'react-icons/ai'
+import { toast } from 'react-toastify'
 
 import * as S from './styles'
 
+type OnSubmitEvent = FormEvent<HTMLFormElement> & {
+  currentTarget: {
+    name: HTMLInputElement,
+    email: HTMLInputElement,
+    password: HTMLInputElement
+  }
+}
+
 function RegisterPage () {
-  const handleSubmit = (e: MouseEvent<HTMLFormElement>) => {
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e: OnSubmitEvent) => {
     e.preventDefault()
-    alert('Submit form!')
+    const { name, email, password } = e.currentTarget
+
+    try {
+      const res = await api.post('/user/create', {
+        name: name.value,
+        email: email.value,
+        password: password.value,
+      })
+
+      toast.success('Registro realizado com sucesso!', {
+        onClose: () => {
+          navigate('/authentication')
+        },
+      })
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        toast.error(error.response?.data.error.message)
+      }
+    }
   }
 
   return (
     <>
       <S.Heading size='large'>Registration</S.Heading>
       <Form onSubmit={handleSubmit}>
-        <Input placeholder='Name' />
-        <Input placeholder='Email' />
-        <Input placeholder='Password' />
+        <Input type='text' placeholder='Name' id='name' />
+        <Input type='email' placeholder='Email' id='email' />
+        <Input type='password' placeholder='Password' id='password' />
 
         <S.ButtonLinkWrapper>
           <ButtonLink
