274e8878062675971adb87f0974871cb368173af
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-03-14 23:19:20 -0300

    feat(pages/AuthPage): added react-hook-form and yup schema validations

--- src/pages/auth-page.tsx
+++ src/pages/auth-page.tsx
@@ -1,7 +1,10 @@
-import { FormEvent, useEffect } from 'react'
+import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useAppDispatch, useAppSelector } from '@/store/hooks'
 import { login, selectAuth, setIsFetching, setError } from '@/features/auth-slice'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
 import { toast } from 'react-toastify'
 
 import { Form } from '@/components/form'
@@ -15,17 +18,28 @@ import {
 
 import * as S from './styles'
 
-type OnSubmitEvent = FormEvent<HTMLFormElement> & {
-  currentTarget: {
-    email: HTMLInputElement
-    password: HTMLInputElement
-  }
+type FormInputs = {
+  email: string
+  password: string
 }
 
+const schema = yup.object({
+  email: yup.string().email().required('Email é obrigatório'),
+  password: yup.string().required('Senha é obrigatória'),
+}).required()
+
 function AuthPage () {
-  const { user, isFetching, error } = useAppSelector(selectAuth)
+  const { user, isFetching, error: errorAuth } = useAppSelector(selectAuth)
   const dispatch = useAppDispatch()
   const navigate = useNavigate()
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors: formErrors },
+  } = useForm<FormInputs>({
+    resolver: yupResolver(schema),
+  })
 
   useEffect(() => {
     if (user) {
@@ -36,32 +50,35 @@ function AuthPage () {
   }, [user, dispatch, navigate])
 
   useEffect(() => {
-    if (error) {
-      toast.error(`${error.message}`, {
+    if (errorAuth) {
+      toast.error(`${errorAuth.message}`, {
         onClose () {
           dispatch(setError(null))
           dispatch(setIsFetching(false))
         },
       })
     }
-  }, [error, dispatch])
+  }, [errorAuth, dispatch])
 
-  const handleSubmit = (e: OnSubmitEvent) => {
-    e.preventDefault()
-    const { email, password } = e.currentTarget
+  useEffect(() => {
+    formErrors.email && toast.error(formErrors.email.message)
+    formErrors.password && toast.error(formErrors.password.message)
+  }, [formErrors])
 
+  const onSubmit = (data: FormInputs) => {
+    const { email, password } = data
     dispatch(login({
-      email: email.value,
-      password: password.value,
+      email,
+      password,
     }))
   }
 
   return (
     <>
       <S.Heading size='large'>Authentication</S.Heading>
-      <Form onSubmit={handleSubmit}>
-        <Input type='email' placeholder='Email' name='email' />
-        <Input type='password' placeholder='Password' name='password' />
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <Input type='email' placeholder='Email' {...register('email')} />
+        <Input type='password' placeholder='Password' {...register('password')} />
 
         <S.ButtonLinkWrapper>
           <ButtonLink
