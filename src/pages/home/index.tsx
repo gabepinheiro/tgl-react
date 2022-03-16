e909be6e9db422c781884cd5af3d4aea77120fe5
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-03-16 13:25:40 -0300

    refactor(pages/home): removed unnecessary import useCallback

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect } from 'react'
+import { useEffect } from 'react'
 import { useAppDispatch, useAppSelector } from '@/store/hooks'
 import { fetchBets, selectBets, setFilteredBets } from '@/features/bets-slice'
 import { selectGames, selectGame, fetchGames } from '@/features/games-slice'
