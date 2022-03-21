c52bb454c78836ecf5e3f3b60bb3d0bf7498e3ea
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-03-20 21:44:13 -0300

    test(cypress/user): added test sign in a user

--- cypress/e2e/user.ts
+++ cypress/e2e/user.ts
@@ -1,7 +1,7 @@
 import { createUser } from '../support/generate'
 
 describe('User', () => {
-  it('should sign up a user', () => {
+  it.skip('should sign up a user', () => {
     cy.visit('/register')
     cy.findByRole('heading', { name: /registration/i }).should('exist')
 
@@ -10,5 +10,17 @@ describe('User', () => {
 
     cy.findByText(/registro realizado com sucesso!/i).should('exist')
     cy.location('href').should('eq', `${Cypress.config().baseUrl}/authentication`)
+  })
+
+  it('should sign in a user', () => {
+    cy.visit('/authentication')
+    cy.findByRole('heading', { name: /authentication/i }).should('exist')
+
+    cy.findByPlaceholderText(/email/i).type('e2e@tgl.com.br')
+    cy.findByPlaceholderText(/password/i).type('123456')
+
+    cy.findByRole('button', { name: /log in/i }).click()
+
+    cy.findByText(/cypress/i).should('exist')
   })
 })
