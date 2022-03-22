fbd06f4247a251dbd277afd858fdd9269da9eefc
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-03-22 10:30:16 -0300

    test(cypress/change-password): refactor tests and added test to 'code is not valid'

--- cypress/e2e/change-password.ts
+++ cypress/e2e/change-password.ts
@@ -1,18 +1,29 @@
 describe('Change password', () => {
-  beforeEach(() => {
-    cy.visit('/reset-password')
-    cy.findByPlaceholderText(/email/i).should('exist').type('e2e@tgl.com.br')
-    cy.findByRole('button', { name: /send link/i }).should('exist').click()
-    cy.shouldCloseToastify()
-    cy.url().should('eq', `${Cypress.config().baseUrl}/change-password`)
-  })
-
   it('should show error if password does not match', () => {
+    cy.visit('/change-password')
+
     cy.findByPlaceholderText(/^new password/i).should('exist').type('123456')
     cy.findByPlaceholderText(/^confirm password/i).should('exist').type('654321')
     cy.findByRole('button', { name: /change/i }).should('exist').click()
 
     cy.findByText(/as senhas não correspondem/i).should('exist')
+  })
+
+  it('should show error if code is not valid', () => {
+    cy.intercept('POST', '**/reset', res => {
+      res.reply({
+        status: 404,
+        body: {},
+      })
+    })
+
+    cy.visit('/change-password?code=wrong_code')
+
+    cy.findByPlaceholderText(/^new password/i).should('exist').type('123456')
+    cy.findByPlaceholderText(/^confirm password/i).should('exist').type('123456')
+    cy.findByRole('button', { name: /change/i }).should('exist').click()
+
+    cy.findByText(/código inválido(\.)?/i).should('exist')
   })
 
   it('should fill password and confirm password, and redirect user to authentication page', () => {
@@ -21,10 +32,12 @@ describe('Change password', () => {
       body: {},
     })
 
+    cy.visit('/change-password?code=valid_code')
+
     cy.findByPlaceholderText(/^new password/i).should('exist').type('123456')
     cy.findByPlaceholderText(/^confirm password/i).should('exist').type('123456')
     cy.findByRole('button', { name: /change/i }).should('exist').click()
-    cy.url().should('eq', `${Cypress.config().baseUrl}/authentication`)
     cy.findByText(/sua senha foi alterada com sucesso(\.)?/i).should('exist')
+    cy.url().should('eq', `${Cypress.config().baseUrl}/authentication`)
   })
 })
